e5aec09f5b552450acfb59163c6aa14b8954b802
colemaxwell <colemaxwell16@gmail.com>
2022-03-02 12:24:44 -0800

    Update GroundUp_Follower.ino

--- GroundUp_Follower/GroundUp_Follower.ino
+++ GroundUp_Follower/GroundUp_Follower.ino
@@ -5,12 +5,39 @@ byte LIMIT_SWITCH_ARM_DOWN;
 #define Ts 20000
 volatile boolean timerFlag;
 IntervalTimer myTimer;
- 
- 
-//IR PINS
-#define LIMIT_SWITCH_ARM_UP_PIN 7
-#define LIMIT_SWITCH_ARM_DOWN_PIN 8
- 
+IntervalTimer UltrasonicTimer;
+#include <NewPing.h>
+
+NewPing sonar[2] = {   // Sensor object array.
+  NewPing(20, 20, 200), // Each sensor's trigger pin, echo pin, and max distance to ping. 
+  NewPing(19, 19, 200), 
+};
+
+//ULTRASONIC PINS
+#define ULTRASONIC_L_PIN 19
+#define ULTRASONIC_R_PIN 20
+#define ULTRASONIC_F_PIN
+
+//CURRENT_PIN
+int PIN = 0;
+
+int LEFT = 0;
+int RIGHT = 1;
+int LEFT_ULTRASONIC = 0;
+int RIGHT_ULTRASONIC = 0;
+
+
+void ultrasonicTimerPin(void) {
+ if (PIN == LEFT) {
+  LEFT_ULTRASONIC = sonar[0].ping_cm();
+  PIN = RIGHT;
+ }
+ else {
+  RIGHT_ULTRASONIC = sonar[1].ping_cm();
+  PIN = LEFT;
+ }
+}
+
 void timerISR(void) {
  timerFlag = true;
 }
@@ -19,18 +46,17 @@ char buffer[5];
  
 void setup() {
  Serial1.begin(38400);
- pinMode(LIMIT_SWITCH_ARM_UP_PIN,INPUT_PULLUP);
- pinMode(LIMIT_SWITCH_ARM_DOWN_PIN,INPUT_PULLUP);
+
  myTimer.begin(timerISR,Ts);
- while(!Serial1);
+ UltrasonicTimer.begin(ultrasonicTimerPin,20000);
+
+ while(Serial1);
 }
  
 void loop() {
- LIMIT_SWITCH_ARM_UP = digitalRead(LIMIT_SWITCH_ARM_UP_PIN);
- LIMIT_SWITCH_ARM_DOWN = digitalRead(LIMIT_SWITCH_ARM_UP_PIN);
   if(timerFlag) {
    timerFlag = false;
-   sprintf(buffer,"%d,%d,%d,%d", LIMIT_SWITCH_ARM_UP,LIMIT_SWITCH_ARM_DOWN,1);
+   sprintf(buffer,"%d,%d,", LEFT_ULTRASONIC,RIGHT_ULTRASONIC);
    Serial1.write(buffer);
    Serial1.flush();
  }
